feadb47e26ee284819bd9f1d395c8cabe6366ddc
Ayon Jodder <ayonjodder177@gmail.com>
2022-07-13 11:00:56 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Products from './Pages/Products/Products';
+import NavMenu from './Pages/Shared/NavMenu';
 
 function App() {
   return (
     <div>
-      
+      <NavMenu/>
     </div>
   );
 }
---
+++ src/Pages/PrductDetails/ProductDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Products.css';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductDetails;
