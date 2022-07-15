d54690d6f4feaf134020aab2d686579b4bc5ec4d
Ayon Jodder <ayonjodder177@gmail.com>
2022-07-15 13:28:04 +0600

    Spinner added

---
+++ src/Pages/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loader text-center">
+            <span class="loader_span"></span>
+        </div>
+    )
+}
+
+export default Spinner
