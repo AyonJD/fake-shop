36142550d8ad7d3cd2cc2c00f7cf86ae19c65f67
Ayon Jodder <ayonjodder177@gmail.com>
2022-07-15 22:14:54 +0600

    Product details fixed

--- src/Pages/PrductDetails/ProductDetails.js
+++ src/Pages/PrductDetails/ProductDetails.js
@@ -1,8 +1,6 @@
 import { AiFillHeart } from 'react-icons/ai';
 import React, { useEffect, useState } from 'react';
 import './ProductDetails.css';
-import { Tabs } from 'react-bootstrap';
-import { Tab } from 'bootstrap';
 import { useParams } from 'react-router-dom';
 import HandleRating from '../HandleRating/HandleRating';
 import Spinner from '../Spinner/Spinner';
@@ -11,6 +9,7 @@ const ProductDetails = () => {
     const { id } = useParams();
     const [spinner, setSpinner] = useState(false);
     const [product, setProduct] = useState({});
+    const { category, description, image, price, title, rating } = product;
 
     useEffect(() => {
         setSpinner(true);
@@ -22,30 +21,31 @@ const ProductDetails = () => {
             })
     }, [id]);
 
-    // console.log(product)
-    const imageOne = <img className='w-25 text-start' src={product?.image} alt="" />;
+    let percentage = (rating?.rate / 5) * 100;
     return (
         <>
             {
                 spinner ? <Spinner /> :
                     <div class="container">
-                        <div class="card">
+                        <div class="card my-5 px-3 py-5">
                             <div class="container-fliud">
                                 <div class="wrapper row">
-                                    <div class="col-md-6">
-                                        <img src={product?.image} alt="" />
+                                    <div class="col-md-6 text-center">
+                                        <img className='w-75' src={product?.image} alt="" />
                                     </div>
 
 
                                     <div class="details col-md-6">
-                                        <h3 class="product-title">men's shoes fashion</h3>
-                                        <div class="">
-                                            <HandleRating rating={product?.rating?.rate} />
-                                            <span class="review-no">41 reviews</span>
+                                        <h3 class="product-title">{title}</h3>
+                                        <div className="d-flex align-items-center">
+                                            <HandleRating rating={rating?.rate} />
+                                            <div className="ml-auto">
+                                                <small className='text-muted fw-bold'>({rating?.count} reviews)</small>
+                                            </div>
                                         </div>
-                                        <p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-                                        <h4 class="price">current price: <span>$180</span></h4>
-                                        <p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
+                                        <p class="product-description card_description">{description}</p>
+                                        <h4 class="price">price: <span>${price}</span></h4>
+                                        <p class="vote"><strong>{percentage}%</strong> of buyers enjoyed this product! <strong>({rating?.count} votes)</strong></p>
                                         <h5 class="sizes">sizes:
                                             <span class="size" data-toggle="tooltip" title="small">s</span>
                                             <span class="size" data-toggle="tooltip" title="medium">m</span>
@@ -57,7 +57,7 @@ const ProductDetails = () => {
                                             <span class="color green"></span>
                                             <span class="color blue"></span>
                                         </h5>
-                                        <div class="action">
+                                        <div class="">
                                             <button class="add-to-cart btn btn-default" type="button">add to cart</button>
                                             <button class="like ms-2 btn btn-default" type="button"><AiFillHeart /></button>
 
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import SingleProductCard from '../SingleProductCard/SingleProductCard';
+import Spinner from '../Spinner/Spinner';
 import './Products.css';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
+    const [spinner, setSpinner] = useState(false);
 
     useEffect(() => {
+        setSpinner(true);
         fetch('https://fakestoreapi.com/products')
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data);
+                setSpinner(false);
+            });
     }, []);
 
-    // console.log(products);
     return (
-        <div className='container'>
-            <div className="row">
-                    {
-                        products.map(product => <SingleProductCard key={product.id} product={product} />)
-                    }
-            </div>
-        </div>
+        <>
+            {
+                spinner ? <Spinner /> :
+                    <div className='container'>
+                        <div className="row">
+                            {
+                                products.map(product => <SingleProductCard key={product.id} product={product} />)
+                            }
+                        </div>
+                    </div>
+            }
+        </>
     );
 };
 
